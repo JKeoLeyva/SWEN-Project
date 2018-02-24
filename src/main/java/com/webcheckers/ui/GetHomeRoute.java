78e5a168c4f6fa493e13a32dbdceae7acb74cf09
Karl Coelho <karl.coelho1@gmail.com>
2018-02-24 16:02:15 -0500

    fix GetHomeRoute formatting

--- src/main/java/com/webcheckers/ui/GetHomeRoute.java
+++ src/main/java/com/webcheckers/ui/GetHomeRoute.java
@@ -18,46 +18,41 @@ import spark.TemplateEngine;
  * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
  */
 public class GetHomeRoute implements Route {
-  private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
+    private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
 
-  private final TemplateEngine templateEngine;
-  private final PlayerLobby playerLobby;
+    private final TemplateEngine templateEngine;
+    private final PlayerLobby playerLobby;
 
-  /**
-   * Create the Spark Route (UI controller) for the
-   * {@code GET /} HTTP request.
-   *
-   * @param templateEngine
-   *   the HTML template rendering engine
-   */
-  public GetHomeRoute(final TemplateEngine templateEngine, final PlayerLobby playerLobby) {
-    // validation
-    Objects.requireNonNull(templateEngine, "templateEngine must not be null");
-    //
-    this.templateEngine = templateEngine;
-    this.playerLobby = playerLobby;
-    //
-    LOG.config("GetHomeRoute is initialized.");
-  }
+    /**
+     * Create the Spark Route (UI controller) for the
+     * {@code GET /} HTTP request.
+     *
+     * @param templateEngine the HTML template rendering engine
+     */
+    public GetHomeRoute(final TemplateEngine templateEngine, final PlayerLobby playerLobby) {
+        // validation
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+        //
+        this.templateEngine = templateEngine;
+        this.playerLobby = playerLobby;
+        //
+        LOG.config("GetHomeRoute is initialized.");
+    }
 
-  /**
-   * Render the WebCheckers Home page.
-   *
-   * @param request
-   *   the HTTP request
-   * @param response
-   *   the HTTP response
-   *
-   * @return
-   *   the rendered HTML for the Home page
-   */
-  @Override
-  public Object handle(Request request, Response response) {
-    LOG.finer("GetHomeRoute is invoked.");
-    //
-    Map<String, Object> vm = new HashMap<>();
-    vm.put("title", "Welcome!");
-    return templateEngine.render(new ModelAndView(vm , "home.ftl"));
-  }
+    /**
+     * Render the WebCheckers Home page.
+     *
+     * @param request  the HTTP request
+     * @param response the HTTP response
+     * @return the rendered HTML for the Home page
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetHomeRoute is invoked.");
+        //
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Welcome!");
+        return templateEngine.render(new ModelAndView(vm, "home.ftl"));
+    }
 
 }
