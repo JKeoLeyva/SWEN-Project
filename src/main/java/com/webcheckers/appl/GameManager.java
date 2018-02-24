85913df473779556a81aa97b9ac85bd16e92c59a
Karl Coelho <karl.coelho1@gmail.com>
2018-02-24 17:56:25 -0500

    Create BoardManager

---
+++ src/main/java/com/webcheckers/appl/GameManager.java
@@ -0,0 +1,23 @@
+package com.webcheckers.appl;
+
+import com.webcheckers.model.Board;
+import com.webcheckers.model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameManager {
+    private List<Board> boards;
+
+    public GameManager() {
+        this.boards = new ArrayList<>();
+    }
+
+    public void createBoard(Board board) {
+        this.boards.add(board);
+    }
+
+    public List<Board> getBoards() {
+        return boards;
+    }
+}
