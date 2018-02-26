d904758cb8b6db79f22597c8598d50efb2209dd8
Sam Davis <sjd8078@rit.edu>
2018-02-26 18:33:47 -0500

    Completed Piece Class

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -1,39 +1,36 @@
 package com.webcheckers.model;
 
-// @author Jacob Keegan
-// A class representing a checkers piece.
-
-import com.webcheckers.appl.Message;
-
 public class Piece {
-    public enum pieceType{
+
+    private Type t;
+    private Color color;
+
+    public enum Type{
         SINGLE, KING
     }
-    public enum pieceColor{
+
+    public enum Color{
         RED, WHITE
     }
 
-    private pieceType type;
-    private pieceColor color;
-
-    public Piece(pieceType type, pieceColor color) {
-        this.type = type;
-        this.color = color;
+    public Piece(Type t, Color c){
+        this.t = t;
+        this.color = c;
     }
 
-    /**
-     * Returns what type of piece this is.
-     * @return the type
-     */
-    public pieceType getType() {
-        return type;
+    public Type getT() {
+        return t;
     }
 
-    /**
-     * Returns what color this piece is.
-     * @return the color
-     */
-    public pieceColor getColor() {
+    public Color getColor() {
         return color;
+    }
+
+    @Override
+    public String toString() {
+        String t_string = this.t == Type.SINGLE ? "Single" : "King";
+        String color_string = this.color == Color.RED ? "Red" : "White";
+
+        return color_string + " " + t_string;
     }
 }
--- src/main/java/com/webcheckers/ui/GetHomeRoute.java
+++ src/main/java/com/webcheckers/ui/GetHomeRoute.java
@@ -8,7 +8,6 @@ import java.util.logging.Logger;
 
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Player;
-import javafx.geometry.Pos;
 import spark.*;
 
 /**
