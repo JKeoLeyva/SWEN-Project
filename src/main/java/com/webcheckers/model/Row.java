037db536ed558e1fef1fa4b8d2c3e43d7c067743
Sam Davis <sjd8078@rit.edu>
2018-02-26 18:34:31 -0500

    Completed Row Class

---
+++ src/main/java/com/webcheckers/model/Row.java
@@ -0,0 +1,49 @@
+package com.webcheckers.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Row implements Iterable<Space>{
+
+    private ArrayList<Space> spaces;
+    private int index;
+
+    public Row(int index){
+        if (index < 0 || index > 7) {
+            throw new IllegalArgumentException("Index must be between 0-7");
+        }
+
+        this.index = index;
+
+        this.spaces = new ArrayList<>();
+        for(int i = 0; i < 8; i++) {
+            spaces.add(new Space(i));
+        }
+    }
+
+    public boolean spaceIsValid(int cellIdx){
+        return spaces.get(cellIdx).isValid(index);
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public Space getSpace(int cellIdx){
+        return spaces.get(cellIdx);
+    }
+
+    @Override
+    public Iterator<Space> iterator() {
+        return spaces.iterator();
+    }
+
+    @Override
+    public String toString() {
+        String all = "";
+        for(Space s : spaces){
+            all += s;
+        }
+        return all;
+    }
+}
