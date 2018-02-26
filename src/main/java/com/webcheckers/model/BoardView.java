001c4847d1bf0c06c54f8453c6823dd65f34ff47
Sam Davis <sjd8078@rit.edu>
2018-02-26 18:35:01 -0500

    Completed BoardView with small test case in main()

---
+++ src/main/java/com/webcheckers/model/BoardView.java
@@ -0,0 +1,60 @@
+package com.webcheckers.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class BoardView implements Iterable<Row> {
+
+    private ArrayList<Row> rows;
+
+    public BoardView(){
+        rows = new ArrayList<>();
+        for(int i = 0; i < 8; i++) {
+            rows.add(new Row(i));
+        }
+    }
+
+    public void populateBoard(){
+        for(int row = 0; row < 8; row++) {
+            for(int col = 0; col < 8; col++) {
+                if(row % 2 == col % 2) {
+                    if(row < 3) {
+                        rows.get(row).getSpace(col).setPiece(new Piece(Piece.Type.SINGLE, Piece.Color.RED));
+                    }
+                    else if(row > 4){
+                        rows.get(row).getSpace(col).setPiece(new Piece(Piece.Type.SINGLE, Piece.Color.WHITE));
+                    }
+                }
+            }
+        }
+    }
+
+    public Space getSpace(int row, int cellIdx){
+        return rows.get(row).getSpace(cellIdx);
+    }
+
+    @Override
+    public Iterator<Row> iterator() {
+        return rows.iterator();
+    }
+
+    @Override
+    public String toString() {
+        String allRows = "";
+        for(int i = 0; i < 8; i++) {
+            allRows += rows.get(i) + "\n";
+        }
+
+        return allRows;
+    }
+
+    public static void main(String[] args) {
+        BoardView b = new BoardView();
+        b.populateBoard();
+        System.out.println(b);
+
+        Space s = new Space(1);
+        s.setPiece(new Piece(Piece.Type.SINGLE, Piece.Color.RED));
+        System.out.println(b.getSpace(0,0));
+    }
+}
