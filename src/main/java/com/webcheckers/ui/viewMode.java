c40ecab97b765f242fbcee49f8e300d7a7bfaec2
JacobKeegan <35943966+jacobkeegan@users.noreply.github.com>
2018-02-26 21:45:10 -0500

    Added viewMode enum.

--- src/main/java/com/webcheckers/ui/viewMode.java
+++ src/main/java/com/webcheckers/ui/viewMode.java
@@ -2,6 +2,7 @@ package com.webcheckers.ui;
 
 // An enum for the type of checkers viewing. May be needed for enhancements.
 // @author Jacob Keegan
+
 public enum viewMode {
     PLAY, SPECTATOR, REPLAY
 }
