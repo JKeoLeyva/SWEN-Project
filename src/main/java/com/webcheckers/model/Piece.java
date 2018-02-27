d45a6025b18c8d08feea3d9d7f62ef30012a2228
Mark Drobnak <mtd8050@rit.edu>
2018-02-27 16:45:14 -0500

    Finish moving Piece, Player, Row, and Space to model
    Signed-off-by: Mark Drobnak <mtd8050@rit.edu>

---
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -0,0 +1,36 @@
+package com.webcheckers.model;
+
+public class Piece {
+
+    public enum Type {
+        SINGLE, KING
+    }
+
+    public enum Color {
+        RED, WHITE
+    }
+
+    private Type type;
+    private Color color;
+
+    public Piece(Type t, Color c) {
+        this.type = t;
+        this.color = c;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        String t_string = this.type == Type.SINGLE ? "Single" : "King";
+        String color_string = this.color == Color.RED ? "Red" : "White";
+
+        return color_string + " " + t_string;
+    }
+}
---
+++ src/main/java/com/webcheckers/model/Row.java
@@ -0,0 +1,37 @@
+package com.webcheckers.model;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Row implements Iterable<Space> {
+
+    private List<Space> spaces;
+    private int index;
+
+    public Row(int index, List<Space> spaces) {
+        if(spaces.size() != Board.BOARD_SIZE) {
+            throw new IllegalArgumentException("Index must be between 0-7");
+        }
+
+        this.index = index;
+        this.spaces = spaces;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public Iterator<Space> iterator() {
+        return spaces.iterator();
+    }
+
+    @Override
+    public String toString() {
+        String all = "";
+        for(Space s : spaces) {
+            all += s;
+        }
+        return all;
+    }
+}
