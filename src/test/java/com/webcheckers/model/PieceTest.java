ef0385422d520915796211e3140a057fb8399fa5
Mark Drobnak <mtd8050@rit.edu>
2018-03-18 17:39:32 -0400

    Add JavaDoc to PieceTest
    Signed-off-by: Mark Drobnak <mtd8050@rit.edu>

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -7,6 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Tag("Model-tier")
 public class PieceTest {
+    /**
+     * Generic test for all Piece configurations
+     *
+     * @param type the piece type
+     * @param color the piece color
+     * @param toString the expected output for toString()
+     */
     private void testPiece(Piece.Type type, Piece.Color color, String toString) {
         // Create the piece
         Piece piece = new Piece(type, color);
@@ -17,21 +24,33 @@ public class PieceTest {
         assertEquals(toString, piece.toString());
     }
 
+    /**
+     * Test the single red piece
+     */
     @Test
     public void testSingleRed() {
         testPiece(Piece.Type.SINGLE, Piece.Color.RED, "Red Single");
     }
 
+    /**
+     * Test the single white piece
+     */
     @Test
     public void testSingleWhite() {
         testPiece(Piece.Type.SINGLE, Piece.Color.WHITE, "White Single");
     }
 
+    /**
+     * Test the king red piece
+     */
     @Test
     public void testKingRed() {
         testPiece(Piece.Type.KING, Piece.Color.RED, "Red King");
     }
 
+    /**
+     * Test the king white piece
+     */
     @Test
     public void testKingWhite() {
         testPiece(Piece.Type.KING, Piece.Color.WHITE, "White King");
