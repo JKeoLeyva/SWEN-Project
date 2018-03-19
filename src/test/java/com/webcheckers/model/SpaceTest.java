eec344c6b971e4ef787a6c8e7cb073cc21cd6e8c
sjd8078 <sjd8078@rit.edu>
2018-03-19 17:35:18 -0400

    Space Unit Test Branch Create

---
+++ src/test/java/com/webcheckers/model/SpaceTest.java
@@ -0,0 +1,57 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpaceTest {
+    /**
+     * Generic test for all Piece configurations
+     *
+     * @param type the piece type
+     * @param color the piece color
+     * @param toString the expected output for toString()
+     */
+    private void testPiece(Piece.Type type, Piece.Color color, String toString) {
+        // Create the piece
+        Piece piece = new Piece(type, color);
+
+        // Verify the piece
+        assertEquals(color, piece.getColor());
+        assertEquals(type, piece.getType());
+        assertEquals(toString, piece.toString());
+    }
+
+    /**
+     * Test the single red piece
+     */
+    @Test
+    public void testSingleRed() {
+        testPiece(Piece.Type.SINGLE, Piece.Color.RED, "Red Single");
+    }
+
+    /**
+     * Test the single white piece
+     */
+    @Test
+    public void testSingleWhite() {
+        testPiece(Piece.Type.SINGLE, Piece.Color.WHITE, "White Single");
+    }
+
+    /**
+     * Test the king red piece
+     */
+    @Test
+    public void testKingRed() {
+        testPiece(Piece.Type.KING, Piece.Color.RED, "Red King");
+    }
+
+    /**
+     * Test the king white piece
+     */
+    @Test
+    public void testKingWhite() {
+        testPiece(Piece.Type.KING, Piece.Color.WHITE, "White King");
+    }
+
+}
