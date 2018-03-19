a57f2439279f483641f9d684dfe37936b4e84a5b
Sam Davis <sjd8078@rit.edu>
2018-03-19 19:06:00 -0400

    Finished tests

--- src/test/java/com/webcheckers/model/SpaceTest.java
+++ src/test/java/com/webcheckers/model/SpaceTest.java
@@ -7,28 +7,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SpaceTest {
 
     /**
-     * Generic test for all Piece configurations
+     * Generic test for all Space configurations
      *
      * @param type the piece type
-     * @param color the piece color
+     * @param row the space row
+     * @param cellIdx the space cell index
      * @param toString the expected output for toString()
      */
-    private void testSpace(Piece.Type type, Piece.Color color, String toString) {
-        // Create the piece
-        Piece piece = new Piece(type, color);
+    private void testSpace(Piece piece, int row, int cellIdx, String toString, boolean isValid) {
 
-        // Verify the piece
-        assertEquals(color, piece.getColor());
-        assertEquals(type, piece.getType());
-        assertEquals(toString, piece.toString());
+        Space space = new Space(row, cellIdx, piece);
+
+        // Verify the space
+        assertEquals(piece, space.getPiece());
+        assertEquals(cellIdx, space.getCellIdx());
+        assertEquals(row, space.getRow());
+        assertEquals(toString, space.toString());
+        assertEquals(isValid, space.isValid());
     }
 
     /**
-     * Test the single red piece
+     * Tests the valid space w/ piece
      */
     @Test
-    public void testSingleRed() {
-
+    public void testValidSpacePiece() {
+        Piece p = new Piece(Piece.Type.SINGLE, Piece.Color.RED);
+        testSpace(p, 1, 1, "[" + p.toString() + "]", false);
     }
 
+    /**
+     * Tests the valid space w/o piece
+     */
+    @Test
+    public void testValidSpaceNoPiece() {
+        testSpace(null, 1, 1, "[ ]", true);
+    }
+
+    /**
+     * Tests the unvalid space
+     */
+    @Test
+    public void testUnvalidSpace() {
+        testSpace(null, 0, 1, "[ ]", false);
+    }
 }
