82c36d4d31b9461684079e554a851b537ea284f7
JacobKeegan <jacobmkeegan@gmail.com>
2018-03-20 11:51:24 -0400

    Fixed BoardView testing.

--- src/test/java/com/webcheckers/ui/BoardViewTest.java
+++ src/test/java/com/webcheckers/ui/BoardViewTest.java
@@ -2,14 +2,14 @@ package com.webcheckers.ui;
 
 import com.webcheckers.model.*;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Tag("UI-tier")
 class BoardViewTest {
 
     private static Board board;
@@ -18,35 +18,41 @@ class BoardViewTest {
     private static BoardView CuT;
     private static Iterator<Row> rowIterator;
 
-    @Test
-    void iterator() {
+    @BeforeAll
+    static void create() {
         player1 = new Player("player1");
         player2 = new Player("player2");
         board = new Board(player1, player2);
-        Row currRow;
-        // Test the normal BoardView orientation.
+    }
+
+    @Test
+    void noBoard(){
+        assertThrows(NullPointerException.class, () ->
+        {new BoardView(null, true);},
+                "Contructed allowed a non-existent board.");
+    }
+
+    // Test the BoardView forward.
+    @Test
+    void iteraorForward(){
         CuT = new BoardView(board, false);
         rowIterator = CuT.iterator();
         for(int row = 0; row < Board.BOARD_SIZE; row++){
-            currRow = rowIterator.next();
-            assertTrue(currRow.getIndex() == row);
-        }
-        assertFalse(rowIterator.hasNext());
-        // Test the BoardView reversed.
-        CuT = new BoardView(board, true);
-        rowIterator = CuT.iterator();
-        for(int row = Board.BOARD_SIZE-1; row >= 0; row--){
-            currRow = rowIterator.next();
+            Row currRow = rowIterator.next();
             assertTrue(currRow.getIndex() == row);
         }
         assertFalse(rowIterator.hasNext());
     }
 
-//    private boolean pieceEquals(Piece piece1, Piece piece2){
-//        if(piece1 == null || piece2 == null)
-//            return piece1 == piece2;
-//        return piece1.getColor() == piece2.getColor() &&
-//                piece1.getType() == piece2.getType();
-//    }
-
+    // Test the BoardView reversed.
+    @Test
+    void iteratorReverse(){
+        CuT = new BoardView(board, true);
+        rowIterator = CuT.iterator();
+        for(int row = Board.BOARD_SIZE-1; row >= 0; row--){
+            Row currRow = rowIterator.next();
+            assertTrue(currRow.getIndex() == row);
+        }
+        assertFalse(rowIterator.hasNext());
+    }
 }
