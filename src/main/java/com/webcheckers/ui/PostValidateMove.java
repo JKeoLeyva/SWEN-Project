fb3cda57e1f21d2bb5623ecfb2dff75d21b22976
JacobKeegan <jacobmkeegan@gmail.com>
2018-03-24 14:51:52 -0400

    Started work on Valid Moves User story.

---
+++ src/main/java/com/webcheckers/ui/PostValidateMove.java
@@ -0,0 +1,16 @@
+package com.webcheckers.ui;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.logging.Logger;
+
+public class PostValidateMove implements Route {
+    private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
+
+    @Override
+    public Object handle(Request request, Response response) {
+        return null;
+    }
+}
