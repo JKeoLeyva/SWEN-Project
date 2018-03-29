1e257786d1eebdf23017020b2d9121a27ea2593e
Karl Coelho <karl.coelho1@gmail.com>
2018-03-29 13:29:37 -0400

    GetGameRouteTest

--- src/test/java/com/webcheckers/ui/GetGameRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetGameRouteTest.java
@@ -1,10 +1,12 @@
 package com.webcheckers.ui;
 
 import com.webcheckers.appl.GameManager;
+import com.webcheckers.model.Board;
 import com.webcheckers.model.Game;
 import com.webcheckers.model.Player;
 import com.webcheckers.model.ViewMode;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import spark.*;
 
@@ -14,6 +16,7 @@ import static org.mockito.Mockito.*;
 /**
  * Created by kac9868 on 3/28/2018.
  */
+@Tag("UI-tier")
 public class GetGameRouteTest {
     private TemplateEngineTester engineTester;
 
@@ -25,30 +28,34 @@ public class GetGameRouteTest {
     private Player player2;
     private TemplateEngine templateEngine;
     private GetGameRoute route;
+    private BoardView boardview;
 
     @BeforeEach
-    void setUp() {
+    void setup() {
         this.request = mock(Request.class);
         this.response = mock(Response.class);
         this.session = mock(Session.class);
         this.gameManager = new GameManager();
-        this.player1 = mock(Player.class);
-        this.player2 = mock(Player.class);
+        this.player1 = new Player("Dank");
+        this.player2 = new Player("Memes");
+
+        gameManager.createGame(player1, player2);
+
+        this.boardview = mock(BoardView.class);
         this.templateEngine = mock(TemplateEngine.class);
 
         route = new GetGameRoute(templateEngine, gameManager);
 
         when(request.session()).thenReturn(session);
-        when(request.session().attribute(PostSigninRoute.PLAYER_ATTR)).thenReturn(null);
+        when(request.session().attribute(PostSigninRoute.PLAYER_ATTR)).thenReturn(player1);
 
         engineTester = new TemplateEngineTester();
         when(templateEngine.render(any(ModelAndView.class))).thenAnswer(engineTester.makeAnswer());
     }
 
     @Test
-    void name() {
-        gameManager.createGame(player1, player2);
-        request.session().attribute(PostSigninRoute.PLAYER_ATTR, player1);
+    void openGame() {
+        session.attribute(PostSigninRoute.PLAYER_ATTR, player1);
 
         route.handle(request, response);
         //verify(response, times(1)).redirect(WebServer.GAME_URL);
@@ -61,6 +68,6 @@ public class GetGameRouteTest {
         engineTester.assertViewModelAttribute("redPlayer", gameManager.getGame(player1).getRedPlayer());
         engineTester.assertViewModelAttribute("whitePlayer", gameManager.getGame(player1).getWhitePlayer());
         engineTester.assertViewModelAttribute("activeColor", gameManager.getGame(player1).getActiveColor());
-        engineTester.assertViewModelAttribute("board", gameManager.getGame(player1).makeBoardView(player1));
+        //engineTester.assertViewModelAttribute("board", gameManager.getGame(player1).makeBoardView(player1));
     }
 }
