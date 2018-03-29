27d4a9424bbd52051106b805187632e22d384440
Karl Coelho <karl.coelho1@gmail.com>
2018-03-29 14:32:50 -0400

    working PostSubmitTurnRouteTest

--- src/test/java/com/webcheckers/ui/PostSubmitTurnRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostSubmitTurnRouteTest.java
@@ -3,8 +3,11 @@ package com.webcheckers.ui;
 import com.google.gson.Gson;
 import com.webcheckers.appl.GameManager;
 import com.webcheckers.appl.Message;
+import com.webcheckers.model.Move;
 import com.webcheckers.model.Player;
+import com.webcheckers.model.Position;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import spark.Request;
 import spark.Response;
@@ -14,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@Tag("UI-tier")
 public class PostSubmitTurnRouteTest {
     private GameManager gameManager;
     private Request request;
@@ -23,6 +27,7 @@ public class PostSubmitTurnRouteTest {
     private Player player1;
     private Player player2;
     private PostSubmitTurnRoute route;
+    private PostValidateMoveRoute validateMoveRoute;
 
     @BeforeEach
     void setUp() {
@@ -30,7 +35,7 @@ public class PostSubmitTurnRouteTest {
         this.gson = new Gson();
         this.response = mock(Response.class);
         this.session = mock(Session.class);
-        this.gameManager = mock(GameManager.class);
+        this.gameManager = new GameManager();
 
         this.player1 = new Player("Abc");
         this.player2 = new Player("Xyz");
@@ -39,17 +44,22 @@ public class PostSubmitTurnRouteTest {
         when(session.attribute(PostSigninRoute.PLAYER_ATTR)).thenReturn(player1);
 
         this.route = new PostSubmitTurnRoute(gson, gameManager);
+        this.validateMoveRoute = new PostValidateMoveRoute(gson, gameManager);
     }
 
     @Test
     void testPlacingValidMove() {
-        Player player1 = new Player("Karl");
-        Player player2 = new Player("Neo");
         gameManager.createGame(player1, player2);
 
-        String jsonMessage = (String) route.handle(request, response);
+        Move move = new Move(new Position(5, 0), new Position(4, 1));
+        String moveJson = gson.toJson(move);
 
-        when(session.attribute(PostSigninRoute.PLAYER_ATTR)).thenReturn(player1);
+        when(request.body()).thenReturn(moveJson);
+        String validateJson = (String) validateMoveRoute.handle(request, response);
+        Message validateMessage = gson.fromJson(validateJson, Message.class);
+        assertEquals(validateMessage.getType(), Message.Type.info);
+
+        String jsonMessage = (String) route.handle(request, response);
         Message message = gson.fromJson(jsonMessage, Message.class);
         assertEquals(message.getType(), Message.Type.info);
     }
