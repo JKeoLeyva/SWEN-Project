a6d88252751500dc840d98b4bd26452ea2a8a42d
Mark Drobnak <mtd8050@rit.edu>
2018-04-11 17:49:03 -0400

    Make Strings an interface of constants and other interfaces
    Gets code coverage to 100% instead of sitting there at 0/7.
    
    Signed-off-by: Mark Drobnak <mtd8050@rit.edu>

--- src/main/java/com/webcheckers/Strings.java
+++ src/main/java/com/webcheckers/Strings.java
@@ -1,40 +1,38 @@
 package com.webcheckers;
 
-public final class Strings {
-    private Strings() {}
-
-    public static final class Session {
-        public static final String PLAYER = "player";
-        public static final String MESSAGE = "message";
+public interface Strings {
+    interface Session {
+        String PLAYER = "player";
+        String MESSAGE = "message";
     }
 
-    public static final class Template {
-        public static final class Home {
-            public static final String FILE_NAME = "home.ftl";
-            public static final String CURRENT_PLAYER = "currentPlayer";
-            public static final String PLAYER_LOBBY = "playerLobby";
-            public static final String GAME_MANAGER = "gameManager";
-            public static final String MESSAGE = "message";
+    interface Template {
+        interface Home {
+            String FILE_NAME = "home.ftl";
+            String CURRENT_PLAYER = "currentPlayer";
+            String PLAYER_LOBBY = "playerLobby";
+            String GAME_MANAGER = "gameManager";
+            String MESSAGE = "message";
         }
 
-        public static final class SignIn {
-            public static final String FILE_NAME = "signin.ftl";
-            public static final String PLAYER_NAME = "name";
+        interface SignIn {
+            String FILE_NAME = "signin.ftl";
+            String PLAYER_NAME = "name";
         }
 
-        public static final class Game {
-            public static final String FILE_NAME = "game.ftl";
-            public static final String CURRENT_PLAYER = "currentPlayer";
-            public static final String VIEW_MODE = "viewMode";
-            public static final String RED_PLAYER = "redPlayer";
-            public static final String WHITE_PLAYER = "whitePlayer";
-            public static final String ACTIVE_COLOR = "activeColor";
-            public static final String BOARD = "board";
+        interface Game {
+            String FILE_NAME = "game.ftl";
+            String CURRENT_PLAYER = "currentPlayer";
+            String VIEW_MODE = "viewMode";
+            String RED_PLAYER = "redPlayer";
+            String WHITE_PLAYER = "whitePlayer";
+            String ACTIVE_COLOR = "activeColor";
+            String BOARD = "board";
         }
 
-        public static final class Help {
-            public static final String FILE_NAME = "help.ftl";
-            public static final String CURRENT_PLAYER = "currentPlayer";
+        interface Help {
+            String FILE_NAME = "help.ftl";
+            String CURRENT_PLAYER = "currentPlayer";
         }
     }
 }
