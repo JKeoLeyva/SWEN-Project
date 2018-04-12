637ea9b1a61c2cfac1a04f542d8b51667d12c617
Karl Coelho <karl.coelho1@gmail.com>
2018-04-11 22:24:36 -0400

    Update ReplayTest for getName

--- src/test/java/com/webcheckers/model/ReplayTest.java
+++ src/test/java/com/webcheckers/model/ReplayTest.java
@@ -18,11 +18,15 @@ public class ReplayTest {
         moves.add(new Move(new Position(1,1), new Position(2,2)));
         Game game = mock(Game.class);
         when(game.getSubmittedMoves()).thenReturn(moves);
+        when(game.getRedPlayer()).thenReturn(new Player("Dank"));
+        when(game.getWhitePlayer()).thenReturn(new Player("Memes"));
 
         Replay replay = new Replay(game);
 
         // Verify the moves were retrieved and stored correctly
         assertEquals(moves, replay.getMoves());
+        assertEquals(replay.getName(game.getRedPlayer()), "You vs. Memes");
+        assertEquals(replay.getName(game.getWhitePlayer()), "You vs. Dank");
 
         //noinspection ResultOfMethodCallIgnored
         verify(game).getSubmittedMoves();
